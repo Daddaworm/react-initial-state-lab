b4bbd60652d62f88a5a27ed80b77da35fe8d8c34
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-05-27 21:08:16 -0400

    Completed assignment

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,37 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+import ImageSlider from './ImageSlider'
+
+
+
+export default class Bomb extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            secondsLeft: this.props.initialCount
+        }
+    }
+
+    // generateBoom = () => {
+    //     if (this.state.secondsLeft > 0) {
+    //         return <h4>{this.state.secondsLeft} seconds before I go boom!</h4>
+    //     }  else {
+    //         return <h1>Boom!</h1>
+
+    //     }
+    // }
+
+
+    render() {
+        return (
+            <div>
+                <p>{this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`}</p>
+            </div>
+        )
+    }
+
+}
+
+
+// React a library of classes. Component is one of those classes. When we call constructor, we are basically overiding or replacing the functions with out our own function that we will create. But, we want to use the functions from the Component, so in order to do that, we call super().
